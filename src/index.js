f15580ad7d80f3ce7cbf6545b4c671be3dd3e146
Harley Clair <60947601+harley-dane@users.noreply.github.com>
2025-03-21 15:36:33 +0100

    add

--- src/index.js
+++ src/index.js
@@ -1,5 +1,88 @@
 import data from './data.js';
 
+const dom = {
+    parent: document.getElementById('parent')
+};
 
-const now = new Date('2023-08-05 13:45:00',);
-console.log(now.getDate());
+const createFlight = (flight) => {
+    //Flight
+    const flightDom = document.createElement('div');
+    flightDom.className = 'flight';
+
+    //Name
+    const name = document.createElement('div');
+    name.className = 'name';
+    name.innerText = flight.name;
+
+    //Plane
+    const plane = document.createElement('div');
+    plane.className = 'plane';
+    plane.innerText = flight.plane;
+
+    //Departure
+    const departureDate = document.createElement('div');
+    departureDate.className = 'departureDate';
+    departureDate.innerText = flight.departureDate;
+
+    //origin
+    const origin = document.createElement('div');
+    origin.className = 'origin';
+    origin.innerText = flight.origin;
+
+    //arrivalDate
+
+    const arrivalDate = document.createElement('div');
+    arrivalDate.className = 'arrivalDate';
+    arrivalDate.innerText = flight.arrivalDate;
+
+    //destination
+
+    const destination = document.createElement('div');
+    destination.className = 'destination';
+    destination.innerText = flight.destination;
+
+    //stops
+    const stops = document.createElement('div');
+    stops.className = 'stop';
+    stops.innerText = flight.stops;
+
+    flightDom.append(
+        name,
+        plane,
+        departureDate,
+        origin,
+        arrivalDate,
+        destination,
+        stops
+    );
+    return flightDom;
+};
+
+// sortFlight
+const sortFlight = (data) => {
+    const organizeFlights = data.flights.sort((a, b) => {
+        return (
+            new Date(a.departureDate).getTime() -
+            new Date(b.departureDate).getTime()
+        );
+    });
+    return organizeFlights;
+};
+
+//loadHandler
+const loadHandler = () => {
+    const organizeFlights = sortFlight(data);
+
+    organizeFlights.forEach((flight) => {
+        const flightDom = createFlight(flight);
+        dom.parent.appendChild(flightDom);
+    });
+};
+
+// loadEvent
+
+const loadEvent = () => {
+    window.addEventListener('load', loadHandler);
+};
+
+loadEvent();
